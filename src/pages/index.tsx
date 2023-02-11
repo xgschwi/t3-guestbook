6a5efbb37194b3ecf53c2bc2b8ab8e3b363c3c0d
Xavier Gschwind <xaviergschwind@gmail.com>
2023-02-10 23:15:47 -0500

    Form to add messages to Guestbook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import { api } from '../utils/api'
+import { useState } from 'react'
 
-const GuestbookEntries = () => {
+const GuestbookEntries: React.FC = () => {
   const {data: guestbookEntries, isLoading } = api.guestbook.getAll.useQuery();
 
   if (isLoading) return <div>Fetching messages...</div>
@@ -20,7 +21,60 @@ const GuestbookEntries = () => {
   )
 }
 
-const Home = () => {
+const Form: React.FC = () => {
+  const [message, setMessage] = useState('');
+  const { data: session, status } = useSession();
+  const utils = api.useContext();
+
+  const postMessage = api.guestbook.postMessage.useMutation({
+    onMutate: async (newEntry) => {
+      await utils.guestbook.getAll.cancel();
+      utils.guestbook.getAll.setData(undefined, (prevEntries) => {
+        if (prevEntries) {
+          return [newEntry, ...prevEntries];
+        } else {
+          return [newEntry]
+        }
+      })
+    },
+    onSettled: async () => {
+      await utils.guestbook.getAll.invalidate();
+    }
+  })
+  if (status !== 'authenticated') return null;
+
+  return (
+    <form
+      className='flex gap-2'
+      onSubmit={(event) => {
+        event.preventDefault();
+        postMessage.mutate({
+          name: session.user?.name as string,
+          message,
+        });
+        setMessage('');
+      }}
+      >
+        <input
+          type='text'
+          className='rounded-md border-2 border-zinc-800 bg-neutral-900 px-4 py-2 focus:outline-none'
+          placeholder='Your message...'
+          minLength={2}
+          maxLength={100}
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+        />
+        <button
+          type='submit'
+          className='rounded-md border-2 border-zinc-800 p-2 focus:outline-none'
+        >
+          Submit
+        </button>
+      </form>
+  )
+}
+
+const Home: React.FC = () => {
   const { data: session, status } = useSession();
 
   if (status === 'loading') {
@@ -40,6 +94,9 @@ const Home = () => {
               }}>
               Logout
             </button>
+            <div className='pt-6'>
+              <Form/>
+            </div>
           </>
         ): (
           <button className='mx-auto block rounded-md bg-neutral-800 py-3 px-6 text-center hover:bg-neutral-700'
