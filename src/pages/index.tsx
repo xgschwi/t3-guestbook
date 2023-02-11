c56da15fa2bf4514c1db2be210480b8829f8d3f8
Xavier Gschwind <xaviergschwind@gmail.com>
2023-02-10 22:56:41 -0500

    Guestbook API and Frontend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,57 @@
 import { signIn, signOut, useSession } from "next-auth/react";
+import { api } from '../utils/api'
+
+const GuestbookEntries = () => {
+  const {data: guestbookEntries, isLoading } = api.guestbook.getAll.useQuery();
+
+  if (isLoading) return <div>Fetching messages...</div>
+
+  return (
+    <div className='flex flex-col gap-4'>
+      {guestbookEntries?.map((entry, idx) => {
+        return (
+          <div key={idx}>
+            <p>{entry.message}</p>
+            <span>- {entry.name}</span>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
 
 const Home = () => {
   const { data: session, status } = useSession();
 
   if (status === 'loading') {
-    return <main>Loading...</main>
+    return <main className='flex flex-col items-center pt-4'>Loading...</main>
   }
 
   return (
-    <main>
-      <h1>Guestbook</h1>
-      <div>
+    <main className='flex flex-col items-center'>
+      <h1 className='text-3xl pt-4'>Guestbook</h1>
+      <div className='pt-10'>
         {session ? (
           <>
-            <p>Hello {session.user?.name}</p>
-            <button onClick={() => {
-              signOut().catch(console.log);
-            }}>
+            <p className='mb-4 text-center'>Hello {session.user?.name}</p>
+            <button className='mx-auto block rounded-md bg-neutral-800 py-3 px-6 text-center hover:bg-neutral-700' 
+              onClick={() => {
+                signOut().catch(console.log);
+              }}>
               Logout
             </button>
           </>
         ): (
-          <button onClick={() => {
-            signIn('discord').catch(console.log)
-          }}>
+          <button className='mx-auto block rounded-md bg-neutral-800 py-3 px-6 text-center hover:bg-neutral-700'
+            onClick={() => {
+              signIn('discord').catch(console.log)
+            }}>
             Login With Discord
           </button>
         )}
+        <div className='pt-10'>
+          <GuestbookEntries/>
+        </div>
       </div>
     </main>
   );
---
+++ src/server/api/routers/guestbook.ts
@@ -0,0 +1,40 @@
+import { z } from 'zod';
+import { createTRPCRouter, publicProcedure, protectedProcedure } from '../trpc';
+
+
+export const guestbookRouter = createTRPCRouter({
+  postMessage: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        message: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        await ctx.prisma.guestbook.create({
+          data: {
+            name: input.name,
+            message: input.message
+          }
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    }),
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    try {
+      return await ctx.prisma.guestbook.findMany({
+        select: {
+          name: true,
+          message: true,
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+    } catch (error) {
+      console.log('error', error)
+    }
+  })
+})
